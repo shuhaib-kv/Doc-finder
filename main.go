fab844f7ab5c0a57c2120213c11ce337b5cd4dab
shuhaib-kv <kvshuhaib2@gmail.com>
2023-02-11 20:30:46 +0530

    added search

--- main.go
+++ main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"imagedisplay/db"
 	"imagedisplay/initializers"
+	"imagedisplay/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
+
+var app = gin.Default()
 
 func init() {
 	db.COnnectDb()
@@ -12,5 +18,7 @@ func init() {
 }
 
 func main() {
-
+	routes.Allroutes(app)
+	port := os.Getenv("PORT")
+	app.Run(port)
 }
