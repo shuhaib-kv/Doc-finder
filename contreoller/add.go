4f04659065060326dd97102d7cf98843f2d26ccd
shuhaib-kv <kvshuhaib2@gmail.com>
2023-02-13 11:32:49 +0530

    finishd

--- contreoller/add.go
+++ contreoller/add.go
@@ -1,37 +1,36 @@
 package contreoller
 
 import (
-	"fmt"
 	"imagedisplay/db"
 	"imagedisplay/models"
 	"net/http"
-	"os"
 	"path/filepath"
 
-	"github.com/asticode/go-astitesseract"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/otiai10/gosseract"
 )
 
 func Add(c *gin.Context) {
-	t, _ := astitesseract.New(astitesseract.Options{Languages: []string{"eng"}})
-	defer t.Close()
-	fmt.Println(t.GetUTF8Text("./testdata/test.png"))
 	Image1, _ := c.FormFile("image")
 	extension := filepath.Ext(Image1.Filename)
+
 	img1 := uuid.New().String() + extension
 	c.SaveUploadedFile(Image1, "./public/images"+img1)
 
 	client := gosseract.NewClient()
 	client.Languages = []string{"eng"}
 
-	os.Setenv("TESSDATA_PREFIX", "/usr/share/tesseract-ocr/4.00/tessdata")
-	defer client.Close()
 	client.SetImage("./public/images" + img1)
 	text, err := client.Text()
 	if err != nil {
-		fmt.Println(err.Error())
+
+		c.JSON(http.StatusOK, gin.H{
+			"status":  false,
+			"message": "fild to convert",
+			"error":   err.Error(),
+		})
+		return
 	}
 	uplode := models.Document{
 		FileData: img1,
@@ -39,8 +38,10 @@ func Add(c *gin.Context) {
 	}
 	db.DBS.Create(&uplode)
 	c.JSON(http.StatusOK, gin.H{
-		"text": text,
+		"status": true,
+		"text":   text,
 	})
+	defer client.Close()
 
 }
 
@@ -57,7 +58,7 @@ func Viewimages(c *gin.Context) {
 
 }
 
-func FindWord(c *gin.Context) {
+func Search(c *gin.Context) {
 	var body struct {
 		word string
 	}
@@ -69,20 +70,22 @@ func FindWord(c *gin.Context) {
 		})
 		return
 	}
-
+	var b []string
 	var Document models.Document
-	db.DBS.Where("text LIKE ?", "%"+body.word+"%").First(&Document)
-	if Document.Text != "" {
+	db.DBS.Where("text ILIKE ?", "%"+body.word+"%").First(&Document).Scan(&b)
+	if b != nil {
 		c.JSON(http.StatusCreated, gin.H{
 			"status":  true,
 			"message": "word fount",
 			"data":    Document.FileData,
 		})
-	} else {
-		c.JSON(http.StatusCreated, gin.H{
-			"status":  false,
-			"message": "word not found",
-			"data":    "",
-		})
+		return
 	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status":  false,
+		"message": "word not found",
+		"data":    body.word,
+	})
+
 }
